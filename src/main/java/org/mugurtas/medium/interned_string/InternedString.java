ee8a9fb69c0515469482ed6125d6770729133edd
ugur <ugur.tas2@gmail.com>
2023-05-03 15:01:11 +0200

    additional example for InternedString

--- src/main/java/org/mugurtas/medium/interned_string/InternedString.java
+++ src/main/java/org/mugurtas/medium/interned_string/InternedString.java
@@ -2,6 +2,11 @@ package org.mugurtas.medium.interned_string;
 
 public class InternedString {
     public static void main(String[] args) {
+        example1();
+        example2();
+    }
+
+    public static void example1() {
         String str1 = "Hello"; // String literal, interned
         String str2 = new String("Hello"); // String object, not interned
         String str3 = str2.intern(); // Explicitly intern str2
@@ -12,6 +17,23 @@ public class InternedString {
         System.out.println(str1 == str3); // true
         System.out.println(str1 == str4); // true
         System.out.println(str1 == str5); // false
+    }
+
+    public static void example2() {
+        String a = "hello";
+        String b = new String("hello");
+        String c = new String("hello").intern();
+        String result = "";
+        if (a == b) {
+            result += "1";
+        }
+        if (a == c) {
+            result += "2";
+        }
+        if (b == c) {
+            result += "3";
+        }
+        System.out.println(result);
 
     }
 }
