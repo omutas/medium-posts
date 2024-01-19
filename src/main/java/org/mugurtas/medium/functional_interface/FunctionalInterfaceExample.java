3b14f80c84dda7df666c5bde1612ea20b0ea31b5
ugur <ugur.tas2@gmail.com>
2024-01-19 13:51:12 +0100

    Additional example for FunctionalInterface annotation

--- src/main/java/org/mugurtas/medium/functional_interface/FunctionalInterfaceExample.java
+++ src/main/java/org/mugurtas/medium/functional_interface/FunctionalInterfaceExample.java
@@ -5,6 +5,10 @@ interface MathOperation {
     int operate(int a, int b);
 }
 
+@FunctionalInterface
+interface ErrorHandler {
+    void handleError();
+}
 public class FunctionalInterfaceExample {
     public static void main(String[] args) {
         MathOperation addition = (a, b) -> a + b;
@@ -12,5 +16,14 @@ public class FunctionalInterfaceExample {
 
         System.out.println("Addition: " + addition.operate(5, 3));
         System.out.println("Subtraction: " + subtraction.operate(5, 3));
+
+        ErrorHandler test1 = () -> System.out.println("test");
+        ErrorHandler test2 = () -> System.out.println("test2");
+        caller(test1);
+        caller(test2);
+    }
+
+    public static void caller(ErrorHandler call) {
+        call.handleError();
     }
 }
